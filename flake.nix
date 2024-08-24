4597054a29c711ca1d0c89d60562092bccce61ff
Vincent Thomas <22414954-vincent_thomas1@users.noreply.gitlab.com>
2024-08-24 18:39:32 +0200

    nice inputs

--- flake.nix
+++ flake.nix
@@ -1,38 +1,37 @@
 {
   description = "vtOS";
   inputs = {
+    # Nixpkgs
     nixpkgs.url = "nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+
+    # NUR
     nur.url = "github:nix-community/NUR";
 
-    nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL/main";
-      inputs.nixpkgs.follows = "nixpkgs";
+    # WSL
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Agenix
+    agenix.url = "github:ryantm/agenix";
+    agenix.inputs = {
+      nixpkgs.follows = "nixpkgs";
+      darwin.follows = "";
     };
 
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # ISO Generations
+    nixos-generators.url = "github:nix-community/nixos-generators";
+    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
 
-    agenix = {
-      url = "github:ryantm/agenix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        darwin.follows = "";
-      };
-    };
+    # Nixvim
+    nixvim.url = "github:nix-community/nixvim/nixos-24.05";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nixvim = {
-      url = "github:nix-community/nixvim/nixos-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    # Catppuccin
     catppuccin.url = "github:catppuccin/nix";
   };
 
@@ -49,10 +48,8 @@
         overlays = builtins.attrValues overlays;
       };
     in {
-      nixosModules.default = import ./modules/nixos;
-      homeManagerModules.default = import ./modules/home;
-
-      inherit overlays;
+      devShells = forAllSystems
+        (system: import ./devShells.nix { inherit inputs system; });
 
       packages = forAllSystems (system:
         import ./pkgs {
@@ -60,8 +57,10 @@
           pkgs = mkPkgs { inherit system; };
         });
 
-      devShells = forAllSystems
-        (system: import ./devShells.nix { inherit inputs system; });
+      inherit overlays;
+
+      nixosModules.default = import ./modules/nixos;
+      homeManagerModules.default = import ./modules/home;
 
       nixosConfigurations = import ./hosts { inherit inputs outputs overlays; };
     };
