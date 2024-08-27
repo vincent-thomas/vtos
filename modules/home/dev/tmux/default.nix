625c37d61ed83c2590fa263a4b9bcdb0c3c0104a
Vincent Thomas <22414954-vincent_thomas1@users.noreply.gitlab.com>
2024-08-28 00:30:07 +0200

    nice config

--- modules/home/dev/tmux/default.nix
+++ modules/home/dev/tmux/default.nix
@@ -3,19 +3,19 @@
   config = {
     programs.tmux = lib.mkIf config.vt.tmux.enable {
       enable = true;
+      keyMode = "vi";
+      baseIndex = 1;
+      mouse = true;
+      terminal = "xterm-256color";
       extraConfig = ''
-        set -g mouse on
-        set -g default-terminal "xterm-256color"
         set-option -ga terminal-overrides ",xterm-256color:Tc"
-        set -g base-index 1           # start windows numbering at 1
-        setw -g automatic-rename on   # rename window to reflect current program
+        set -g automatic-rename on   # rename window to reflect current program
         set -g renumber-windows on    # renumber windows when a window is closed
 
         bind-key -r f run-shell "tmux neww ${pkgs.tmux-sessionizer}/bin/tmux-sessionizer"
         bind r source-file ~/.config/tmux/tmux.conf
         bind D detach
         bind d switch-client -t root
-        bind U run "cut -c3- '#{TMUX_CONF}' | sh -s _urlview '#{pane_id}'"
       '';
     };
   };
