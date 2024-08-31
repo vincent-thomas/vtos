8c9f7a04e85812021de260b8cf097c958d2e55d7
Vincent Thomas <22414954-vincent_thomas1@users.noreply.gitlab.com>
2024-08-31 14:36:17 +0200

    tes

--- pkgs/powertools.nix
+++ pkgs/powertools.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 pkgs.writeShellScriptBin "powertools" ''
-  chosen=$(printf "Sleep\nPower Off\nRestart\nLock" | fuzzel --dmenu -i)
+  chosen=$(printf "Hibernate\nPower Off\nRestart\nLock" | fuzzel --dmenu -i)
 
   case "$chosen" in
-    "Sleep") systemctl hibernate ;;
+    "Hibernate") systemctl hibernate ;;
     "Power Off") poweroff ;;
     "Restart") reboot ;;
     "Lock") hyprctl dispatch exit 1 ;;
