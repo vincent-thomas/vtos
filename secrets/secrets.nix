416f41ee94a8d3e1d5f4524ef43d3d944ffb4d54
Vincent Thomas <22414954-vincent_thomas1@users.noreply.gitlab.com>
2024-09-02 16:59:07 +0200

    new vt-password

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,8 @@ let
   vt-pc =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINEuTOYOZwE/eXMAiDVLyTYJ8njbE8/1PdcFtP+H9eJI";
   vt-skol-laptop =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPiKky4l/cRjmi6InvjTQkZYHPOwE4XOFNf7GJecwSzc";
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH19aYJ/sPJBeemeCwq3Xz0FfH9X2YyAxMInYwIKzf5I";
   vt-laptop =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJWEvtG8UCEcFYcYsl47AhSZvghpzJKDGPrPztxh0dFA";
+    
 in { "vt-password.age".publicKeys = [ vt-pc vt-skol-laptop vt-laptop ]; }
