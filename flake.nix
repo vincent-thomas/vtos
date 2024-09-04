80ffd4067f98f61b0ef6930dc1eb9edf8841c129
Vincent Thomas <22414954-vincent_thomas1@users.noreply.gitlab.com>
2024-09-04 13:47:04 +0200

    test ci cache

--- flake.nix
+++ flake.nix
@@ -1,4 +1,17 @@
 {
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://vtos.cachix.org"
+
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "vtos.cachix.org-1:ixP7qoCKWViIMMGj+RDMnTuqoi3N9+D9u7Z+Ty9SYcQ="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
