51e0febbacea6a3c75275a5c5b469dcb34917ef9
Vincent Thomas <22414954-vincent_thomas1@users.noreply.gitlab.com>
2024-09-11 01:26:24 +0200

    update wallpaper repo

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
     deploy-rs.url = "github:serokell/deploy-rs";
     deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
 
-    vt-wallpapers.url = "gitlab:vincent_thomas1/wallpapers";
+    vt-wallpapers.url = "github:vincent-thomas/wallpapers";
     vt-wallpapers.flake = false;
 
     vt-nvim.url = "github:vincent-thomas/nvim";
