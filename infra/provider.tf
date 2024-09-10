3a57122165b1187ab73a5dd0bcc357e931d48e32
Vincent Thomas <22414954-vincent_thomas1@users.noreply.gitlab.com>
2024-09-10 23:16:29 +0200

    yes

--- infra/provider.tf
+++ infra/provider.tf
@@ -4,19 +4,43 @@ terraform {
       source = "hashicorp/dns"
       version = "3.1.0"
     }    
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc4"
+    }
   }
 }
 
-variable "tsig_key" {
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_api_token_id" {
+  type = string
+}
+
+variable "proxmox_api_token_secret" {
   type = string
   sensitive = true
 }
 
-provider "dns" {
-  update {
-    server = ""
-    key_name = ""
-    key_algorithm = ""
-    key_secret = var.tsig_key
-  }
+provider "proxmox" {
+  pm_api_url = var.proxmox_api_url
+  pm_api_token_id = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_tls_insecure = true
 }
+
+# variable "tsig_key" {
+#   type = string
+#   sensitive = true
+# }
+#
+# provider "dns" {
+#   update {
+#     server = ""
+#     key_name = ""
+#     key_algorithm = ""
+#     key_secret = var.tsig_key
+#   }
+# }
