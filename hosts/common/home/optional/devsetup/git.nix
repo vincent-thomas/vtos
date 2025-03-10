29e5219d8310d55751f8ded31174855ce4f13043
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2025-03-10 02:27:01 +0100

    fixed git weird issue

--- hosts/common/home/optional/devsetup/git.nix
+++ hosts/common/home/optional/devsetup/git.nix
@@ -1,8 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = with pkgs; [
     git-lfs
   ];
+
+  # For some reason nixos puts the config under ~/.config/git/config??
   programs.git = {
     enable = true;
     userName = "Vincent Thomas";
@@ -18,6 +20,10 @@
       core.askpass = ""; # Disable git credential manager
       gpg.format = "ssh";
       commit.gpgsign = true;
+      user = {
+        email = "77443389+vincent-thomas@users.noreply.github.com";
+        signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK2TanW6Iiz6qrI+BV8P3KGAfS+w/eKHBoOUiPyBFSal";
+      };
       filter.lfs = {
         clean = "git-lfs clean -- %f";
         smudge = "git-lfs smudge -- %f";
