6e2e44b1f55d81c2cb3c164d1335c5b1d22a19d1
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2025-03-21 01:04:19 +0100

    update neovim

--- hosts/common/home/optional/devsetup/tmux.nix
+++ hosts/common/home/optional/devsetup/tmux.nix
@@ -7,7 +7,7 @@
     mouse = true;
     terminal = "xterm-256color";
     extraConfig = ''
-      set -ga terminal-overrides ",xterm-256color:Tc" # Color issue fix.
+      set -sa terminal-overrides ",xterm*:Tc" # Color issue fix.
 
       set -g automatic-rename on # rename window to reflect current program
       set -g renumber-windows on # renumber windows when a window is closed
